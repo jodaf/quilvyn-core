57a52a2839063d1f662566e430d2be64b04427bd
Jim <Jim>
2004-01-02 21:47:36 +0000

    Initial revision

---
+++ core/RuleEngine.js
@@ -0,0 +1,169 @@
+/*
+Copyright 2002, James J. Hayes
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+
+/*
+ * The RuleEngine class applies a set of client-defined rules that generate a
+ * set of attributes from an initial set.
+ */
+function RuleEngine() {
+  this.sources = { };
+  this.targets = { };
+}
+
+
+/*
+ * Adds a rule to the RuleSet.  #target# is the attribute affected by the rule,
+ * and each #source# an attribute whose value affects #target#.  The latter may
+ * be null, indicating a rule with no source attribute, e.g., a rule that sets
+ * an initial or default value that other rules modify.  #expr# is an
+ * expression that computes the value used to modify #target#.  In most cases
+ * this will be a string containing a JavaScript expression; within this
+ * expression the variable 'source' can be used to reference the current value
+ * of the source attribute.  For complex rules, #expr# can be a function that
+ * takes the value of the source attribute as its sole parameter.  A null
+ * #expr# is assumed to return the current value of the source attribute.  Six
+ * types of rules are supported.  Assignment rules (type '=') set the target to
+ * the #expr# value.  Addition rules (type '+') add the #expr# value to the
+ * target.  Multiplication rules (type '*') multiply the target by the #expr#
+ * value.  Maximum rules (type 'v') lower the value of the target to the #expr#
+ * value if it is greater.  Minumum rules (type '^') raise the value of the
+ * target to the #expr# value if it is less.  Prerequisite rules (type '?') set
+ * the value of the target to null and suppress application of other rules if
+ * #expr# is false.
+ */
+RuleEngine.prototype.AddRules =
+  function(target, source, type, expr /*, source, type, expr ... */) {
+  for(var i = 3; i < arguments.length; i += 3) {
+    source = arguments[i - 2];
+    type = arguments[i - 1];
+    expr = arguments[i];
+    if(source == null)
+      source = '';
+    if(typeof(expr) == 'string')
+      expr = new Function('source', 'return ' + expr + ';');
+    if(this.sources[target] == null)
+      this.sources[target] = { };
+    if(this.targets[source] == null)
+      this.targets[source] = { };
+    this.sources[target][source] = this.targets[source][target] =
+      {type: type, fn: expr};
+  }
+};
+
+
+/*
+ * Invokes the rules that are affected directly or indirectly by the attributes
+ * in the object #initial#.  Returns an object that contains the attributes in
+ * #initial# plus any computed attributes.
+ */
+RuleEngine.prototype.Apply = function(initial) {
+  var computed = { };
+  for(var a in initial)
+    this._Recompute(initial, computed, a);
+  return computed;
+};
+
+
+/* Removes the rule specifying how #source# affects #target#. */
+RuleEngine.prototype.DeleteRule = function(target, source) {
+  delete this.targets[source][target];
+  delete this.sources[target][source];
+};
+
+
+/* Returns true iff the value of #attribute# affects other attributes. */
+RuleEngine.prototype.IsSource = function(attribute) {
+  return this.targets[attribute] != null;
+}
+
+
+/* Returns true iff the value of #attribute# is affected by other attributes. */
+RuleEngine.prototype.IsTarget = function(attribute) {
+  return this.sources[attribute] != null;
+}
+
+
+/*
+ * A "private" function.  Invokes the rules that have #attribute# as their
+ * target, drawing the initial value for #attribute# from #initial# and storing
+ * the computed result in #computed#.  If the computed value changes,
+ * recursively recomputes other attributes that #attribute# affects.
+ */
+RuleEngine.prototype._Recompute = function(initial, computed, attribute) {
+
+  var addition = 0;
+  var max = null;
+  var min = null;
+  var multiplier = 1;
+  var value = initial[attribute];
+
+  var sources = this.sources[attribute];
+  if(sources != null) {
+    for(var source in sources) {
+      var attr = computed[source];
+      if(attr == null)
+        attr = initial[source];
+      if(typeof(attr) == 'string' && attr.length > 0 && !isNaN(attr - 0))
+        attr -= 0; /* Convert string to number. */
+      var fn = sources[source].fn;
+      var type = sources[source].type;
+      var amount = fn != null && (attr!=null || source=='') ? fn(attr) : attr;
+      if(type == '?') {
+        if(!amount) {
+          value = null;
+          break;
+        }
+      }
+      else if(amount == null)
+        continue;
+      else if(type == '=')
+        value = amount;
+      else if(type == '+')
+        addition += amount - 0;
+      else if(type == '*')
+        multiplier *= amount;
+      else if(type == 'v' && (max == null || max > amount))
+        max = amount;
+      else if(type == '^' && (min == null || min < amount))
+        min = amount;
+    }
+  }
+
+  if(value != null) {
+    if(addition != 0)
+      value = (value - 0) + addition;
+    if(multiplier != 1)
+      value *= multiplier;
+    if(max != null && value > max)
+      value = max;
+    if(min != null && value < min)
+      value = min;
+  }
+
+  if(value != computed[attribute]) {
+    if(value == null)
+      delete computed[attribute];
+    else
+      computed[attribute] = value;
+    if(this.targets[attribute] != null) {
+      for(var target in this.targets[attribute])
+        this._Recompute(initial, computed, target);
+    }
+  }
+
+};
---
+++ core/Scribe.js
@@ -0,0 +1,209 @@
+/*
+Copyright 2002, James J. Hayes
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place, Suite 330, Boston, MA 02111-1307 USA.
+*/
+
+var character = null;
+var cookieName = "scribePrefs";
+var editFrame;
+var loadFrame;
+var loadFrameContents = '';
+var preferences = {
+  prefix: '',
+  ruleUrls: '',
+  suffix: '.html'
+};
+var sheetFrame;
+
+function AddRules() {
+  DndCharacter.rules.AddRules.apply(DndCharacter.rules, arguments);
+}
+
+function FullUrl(url) {
+  if(url.match(/[a-zA-Z0-9]*:/) == null)
+    url = preferences.prefix + url;
+  if(url.match(/\.[a-zA-Z0-9]*$/) == null)
+    url += preferences.suffix;
+  return url;
+}
+
+function ChangePreferences() {
+  var cookie = '';
+  var value;
+  for(var p in preferences) {
+    if((value = prompt('Enter value for ' + p, preferences[p])) != null)
+      preferences[p] = value;
+    cookie += p + '\n' + preferences[p] + '\n';
+  }
+  cookie = cookieName + '=' + escape(cookie);
+  var nextYear = new Date();
+  nextYear.setFullYear(nextYear.getFullYear() + 1);
+  cookie += ';expires=' + nextYear.toGMTString();
+  document.cookie = cookie;
+}
+
+function LoadRuleUrls(urls) {
+  var end, url;
+  for( ; urls != ''; urls = urls.substring(end + 1)) {
+    if((end = urls.indexOf(';')) < 0)
+      end = urls.length;
+    if((url = urls.substring(0, end)) == '')
+      continue;
+    url = FullUrl(url);
+    if(loadFrameContents != url) {
+      loadFrame.CustomizeScribe = null;
+      loadFrame.location = loadFrameContents = url;
+      if(loadFrame.CustomizeScribe == null) {
+        setTimeout('LoadRuleUrls("' + urls + '");', 1000);
+        return;
+      }
+    }
+    if(loadFrame.CustomizeScribe == null)
+      alert('Unable to load customizations from "' + url + '"');
+    else {
+      loadFrame.CustomizeScribe
+        (DndCharacter, DndCharacter.AddChoices, AddRules,
+         DndCharacter.AddToSheet);
+      alert('Finished loading customizations from "' + url + '"');
+    }
+    loadFrameContents = '';
+  }
+  RefreshDisplay();
+}
+
+function ReloadRules() {
+  DndCharacter.rules = new RuleEngine();
+  DndCharacter.ThirdEditionRules();
+  LoadRuleUrls(preferences.ruleUrls);
+}
+
+function OpenDialog() {
+  var url = prompt('Enter URL to Edit (Blank for Random Character)', '');
+  if(url == '') {
+    character = new DndCharacter(null);
+    for(var a in DndCharacter.defaults)
+      character.Randomize(a);
+    character.Randomize('class');
+    character.Randomize('feats');
+    character.Randomize('skills');
+    character.Randomize('spells');
+    sheetFrame.attributes = character.attributes;
+  }
+  else {
+    sheetFrame.attributes = null;
+    sheetFrame.location = FullUrl(url);
+  }
+  RefreshDisplay();
+}
+
+function RefreshDisplay() {
+  if(sheetFrame.attributes == null) {
+    setTimeout("RefreshDisplay();", 1000);
+    return;
+  }
+  character = new DndCharacter(sheetFrame.attributes);
+  sheetFrame.document.write(SheetHtml());
+  sheetFrame.document.close();
+  editFrame.document.write(
+    '<html><head><title>Editor</title></head>\n' +
+    '<body>' + character.CharacterEditor(ObjectEditor, 'parent.Update') +
+    '</body></html>\n'
+  );
+  editFrame.document.close();
+}
+
+function ScribeLoaded() {
+  if(window.opener == null || window.opener.ScribeLoaded == null) {
+    alert('Copyright 2002 James J. Hayes\n' +
+          'Press the "About" button for more info');
+    window.open(document.location, 'scribeEditor');
+  }
+  else {
+    var i = document.cookie.indexOf(cookieName + '=');
+    if(i >= 0) {
+      var end = document.cookie.indexOf(';', i);
+      if(end < 0)
+        end = document.cookie.length;
+      var cookie = document.cookie.substring(i + cookieName.length + 1, end);
+      var settings = unescape(cookie).split('\n');
+      for(i = 0; i < settings.length && settings[i] != ''; i += 2)
+        preferences[settings[i]] = settings[i + 1];
+    }
+    editFrame = window.frames[0];
+    loadFrame = window.frames[1];
+    sheetFrame = window.opener.frames[0];
+    ReloadRules();
+    OpenDialog();
+  }
+}
+
+function SheetHtml() {
+  var attrImage = '';
+  for(var a in character.attributes)
+    if(character.attributes[a] != DndCharacter.defaults[a])
+      attrImage += '"' + a + '":"' + character.attributes[a] + '",'
+  attrImage = '{' + attrImage.replace(/,$/, '') + '}';
+  return '<html>\n' +
+         '<head>\n' +
+         '  <title>' + character.attributes.name + '</title>\n' +
+         '  <script>\n' +
+         '    var attributes = ' + attrImage + ';\n' +
+         '  </' + 'script>\n' +
+         '</head>\n' +
+         '<body>\n' +
+         character.CharacterSheet() +
+         '</body>\n' +
+         '</html>\n';
+}
+
+function Update(attr, value) {
+  if(attr == 'meta') {
+    if(value == 'About')
+      window.open('about.html', 'about');
+    else if(value == 'Help')
+      window.open('help.html', 'help');
+    else if(value == 'Preferences') {
+      var oldUrls = preferences.ruleUrls;
+      ChangePreferences();
+      if(preferences.ruleUrls != oldUrls)
+        ReloadRules();
+    }
+    else if(value == 'New/Open')
+      OpenDialog();
+    else if(value == 'View Html') {
+      var html = SheetHtml();
+      var htmlWindow = window.open('about:blank', 'html');
+      html = html.replace(/</g, '&lt;');
+      html = html.replace(/>/g, '&gt;');
+      htmlWindow.document.write(
+        '<html><head><title>HTML</title></head>\n' +
+        '<body><pre>' + html + '</pre></body></html>\n'
+      );
+      htmlWindow.document.close();
+    }
+    return;
+  }
+  else if(attr == 'randomize') {
+    character.Randomize(value);
+    sheetFrame.attributes = character.attributes;
+    RefreshDisplay();
+  }
+  else if(value == '0' && attr.indexOf('.') >= 0)
+    delete character.attributes[attr];
+  else
+    character.attributes[attr] = value;
+  sheetFrame.document.write(SheetHtml());
+  sheetFrame.document.close();
+}
