1b336addb00fe306d38c339acaf1c5b8a9f14447
Jim <Jim>
2004-09-07 06:05:01 +0000

    Better way of suppressing zero notes.

--- srd35/CustomSpellPoints.js
+++ srd35/CustomSpellPoints.js
@@ -17,8 +17,26 @@ Place, Suite 330, Boston, MA 02111-1307 USA.
 
 
 function CustomizeSpellPoints(AddElement, AddRules, AddToSheet) {
-  AddRules('spellPoints', 'sPoints', '=', 'source <= 0 ? null : source');
-  AddRules('sPoints',
+  AddRules('magicNotes.charismaSpellPointsModifier',
+    null, '=', '0',
+    'levels.Bard', '+', null,
+    'levels.Sorcerer', '+', null,
+    'charismaModifier', '*', null
+  );
+  AddRules('magicNotes.intelligenceSpellPointsModifier',
+    null, '=', '0',
+    'levels.Wizard', '+', null,
+    'intelligenceModifier', '*', null
+  );
+  AddRules('magicNotes.wisdomSpellPointsModifier',
+    null, '=', '0',
+    'levels.Cleric', '+', null,
+    'levels.Druid', '+', null,
+    'levels.Paladin', '+', 'source > 3 ? (source - 3) : null',
+    'levels.Ranger', '+', 'source > 3 ? (source - 3) : null',
+    'wisdomModifier', '*', null
+  );
+  AddRules('_spellPoints',
     null, '=', '0',
     'levels.Bard', '+', 'source * (source + 1) / 2',
     'levels.Cleric', '+', 'source * (source + 1) / 2',
@@ -27,46 +45,10 @@ function CustomizeSpellPoints(AddElement, AddRules, AddToSheet) {
     'levels.Ranger', '+', 'source > 3 ? (source - 3) * (source - 2) / 2 : null',
     'levels.Sorcerer', '+', 'source * (source + 1) / 2',
     'levels.Wizard', '+', 'source * (source + 1) / 2',
-    'magicNotes.bardChaSPModifier', '+', null,
-    'magicNotes.clericWisSPModifier', '+', null,
-    'magicNotes.paladinWisSPModifier', '+', null,
-    'magicNotes.rangerWisSPModifier', '+', null,
-    'magicNotes.sorcererChaSPModifier', '+', null,
-    'magicNotes.wizardIntSPModifier', '+', null,
-    'magicNotes.abilitySpellPointsBonus', '+', null
+    'magicNotes.charismaSpellPointsModifier', '+', null,
+    'magicNotes.intelligenceSpellPointsModifier', '+', null,
+    'magicNotes.wisdomSpellPointsModifier', '+', null
   );
-  AddRules('chaMod', 'charismaModifier', '=', 'source == 0 ? null : 1');
-  AddRules('intMod', 'intelligenceModifier', '=', 'source == 0 ? null : 1');
-  AddRules('wisMod', 'wisdomModifier', '=', 'source == 0 ? null : 1');
-  AddRules('magicNotes.bardChaSPModifier',
-    'chaMod', '?', null,
-    'levels.Bard', '=', null,
-    'charismaModifier', '*', null
-  );
-  AddRules('magicNotes.sorcererChaSPModifier',
-    'chaMod', '?', null,
-    'levels.Sorcerer', '=', null,
-    'charismaModifier', '*', null
-  );
-  AddRules('magicNotes.wizardIntSPModifier',
-    'intMod', '?', null,
-    'levels.Wizard', '=', null,
-    'intelligenceModifier', '*', null
-  );
-  AddRules('magicNotes.clericWisSPModifier',
-    'wisMod', '?', null,
-    'levels.Cleric', '=', null,
-    'wisdomModifier', '*', null
-  );
-  AddRules('magicNotes.paladinWisSPModifier',
-    'wisMod', '?', null,
-    'levels.Paladin', '=', null,
-    'wisdomModifier', '*', null
-  );
-  AddRules('magicNotes.rangerWisSPModifier',
-    'wisMod', '?', null,
-    'levels.Ranger', '=', null,
-    'wisdomModifier', '*', null
-  );
+  AddRules('spellPoints', '_spellPoints', '=', 'source == 0 ? null : source');
   AddToSheet('spellPoints', 'Magic', 'Domains', null);
 }
