2625ba8a4836c0dc2faa6c7be913dfc754faef1f
Jim <Jim>
2005-07-26 17:43:17 +0000

    Update version.

--- core/Scribe.js
+++ core/Scribe.js
@@ -1,7 +1,7 @@
-/* $Id: Scribe.js,v 1.113 2005/07/22 05:40:31 Jim Exp $ */
+/* $Id: Scribe.js,v 1.114 2005/07/26 17:43:17 Jim Exp $ */
 
 var COPYRIGHT = 'Copyright 2005 James J. Hayes';
-var VERSION = '0.19.21';
+var VERSION = '0.19.26';
 var ABOUT_TEXT =
 'Scribe Character Editor version ' + VERSION + '\n' +
 'The Scribe Character Editor is ' + COPYRIGHT + '\n' +
