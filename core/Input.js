f3dac68486310254b4d6da9467d71f9d911f42c4
Jim <Jim>
2006-12-30 05:22:33 +0000

    Begin review of MN class rules.
    SKIPPED:
    	lastage/LastAge.js
    	lastage/LastAgePrestige.js

--- core/Input.js
+++ core/Input.js
@@ -1,4 +1,4 @@
-/* $Id: Input.js,v 1.6 2006/12/27 17:32:34 Jim Exp $ */
+/* $Id: Input.js,v 1.7 2006/12/30 05:22:33 Jim Exp $ */
 
 /*
 Copyright 2005, James J. Hayes
@@ -74,8 +74,16 @@ function InputSetOptions(input, options) {
     input.options.length = options.length;
   for(i = 0; i < input.options.length; i++)
     input.options[i].text = input.options[i].value = options[i];
-  for( ; i < options.length; i++)
-    input.options[i] = new Option(options[i], options[i], 0, 0);
+  for( ; i < options.length; i++) {
+    var opt = new Option(options[i], options[i], 0, 0);
+    // Workaround for IE, which disallows adding options created in one
+    // frame/window to a select box in another frame/window.
+    if(input.document != null && input.document.createElement != null) {
+      opt = input.document.createElement("OPTION");
+      opt.text = opt.value = options[i];
+    }
+    input.options[i] = opt;
+  }
   if(options.length > 0)
     input.selectedIndex = 0;
 };
